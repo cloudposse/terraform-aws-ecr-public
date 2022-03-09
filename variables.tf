64aaeede6066ea5c98fd20b3682bd2ef83dd3a33
Matt Calhoun <matt@mattcalhoun.com>
2022-03-09 14:09:55 -0500

    Update variables.tf
    Co-authored-by: Yonatan Koren <me@yonatankoren.com>

--- variables.tf
+++ variables.tf
@@ -18,27 +18,27 @@ variable "principals_readonly_access" {
 
 variable "repository_configs" {
   description = <<EOF
-  name = List of Docker local image names, used as repository names for AWS ECR 
+  `name`: List of Docker local image names, used as repository names for AWS ECR 
   
-  description = A short description of the contents of the repository. This text appears in both the image details and 
+  `description`: A short description of the contents of the repository. This text appears in both the image details and 
   also when searching for repositories on the Amazon ECR Public Gallery.
 
-  about_text = A detailed description of the contents of the repository. It is publicly visible in the Amazon ECR Public 
+  `about_text`: A detailed description of the contents of the repository. It is publicly visible in the Amazon ECR Public 
   Gallery. The text must be in markdown format.
 
-  usage_text = Detailed information on how to use the contents of the repository. It is publicly visible in the Amazon 
+  `usage_text`: Detailed information on how to use the contents of the repository. It is publicly visible in the Amazon 
   ECR Public Gallery. The usage text provides context, support information, and additional usage details for users of 
   the repository. The text must be in markdown format.
 
-  architectures = The system architecture that the images in the repository are compatible with. On the Amazon ECR 
+  `architectures`: The system architecture that the images in the repository are compatible with. On the Amazon ECR 
   Public Gallery, the following supported architectures will appear as badges on the repository and are used as search 
   filters: ARM, ARM 64, x86, x86-64
 
-  operating_systems = The operating systems that the images in the repository are compatible with. On the Amazon ECR 
+  `operating_systems`: The operating systems that the images in the repository are compatible with. On the Amazon ECR 
   Public Gallery, the following supported operating systems will appear as badges on the repository and are used as 
   search filters: Linux, Windows
 
-  logo_image_blob = The base64-encoded repository logo payload. (Only visible for verified accounts) Note that drift 
+  `logo_image_blob`: The base64-encoded repository logo payload. (Only visible for verified accounts) Note that drift 
   detection is disabled for this attribute.
 
   EOF
