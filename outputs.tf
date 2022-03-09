971018226229a9b1f780366a15d24e0fc48b9500
Matt Calhoun <matt@mattcalhoun.com>
2022-03-09 13:02:02 -0500

    update metadata

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
 output "registry_id" {
-  value       = module.this.enabled ? aws_ecrpublic_repository.this[local.image_names[0]].registry_id : ""
+  value       = module.this.enabled ? aws_ecrpublic_repository.this[var.repository_configs[0].name].registry_id : ""
   description = "Registry ID"
 }
 
 output "repository_name" {
-  value       = module.this.enabled ? aws_ecrpublic_repository.this[local.image_names[0]].repository_name : ""
+  value       = module.this.enabled ? aws_ecrpublic_repository.this[var.repository_configs[0].name].repository_name : ""
   description = "Name of first repository created"
 }
 
 output "repository_url" {
-  value       = module.this.enabled ? aws_ecrpublic_repository.this[local.image_names[0]].repository_uri : ""
+  value       = module.this.enabled ? aws_ecrpublic_repository.this[var.repository_configs[0].name].repository_uri : ""
   description = "URL of first repository created"
 }
 
 output "repository_arn" {
-  value       = module.this.enabled ? aws_ecrpublic_repository.this[local.image_names[0]].arn : ""
+  value       = module.this.enabled ? aws_ecrpublic_repository.this[var.repository_configs[0].name].arn : ""
   description = "ARN of first repository created"
 }
 
